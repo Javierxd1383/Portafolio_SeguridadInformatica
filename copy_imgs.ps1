19c15efb71703e1d7c92ea98cd5f3720934ba111
Javierxd1383 <javiercruzz1383@gmail.com>
2026-02-20 15:58:20 -0600

    Agrega imagenes actividad2, actividad3 y actividad6; nuevos archivos actividad6, actividad7, pr01 y PDFs

---
+++ copy_imgs.ps1
@@ -0,0 +1,5 @@
+Copy-Item 'c:\Users\DELL\.gemini\antigravity\brain\c2a0bbe7-5235-47fb-a401-9a7844ce009e\uploaded_media_1_1770756057846.png' 'img\actividad1\evidence2.png' -Force
+Copy-Item 'c:\Users\DELL\.gemini\antigravity\brain\c2a0bbe7-5235-47fb-a401-9a7844ce009e\uploaded_media_2_1770756057846.png' 'img\actividad1\evidence3.png' -Force
+Copy-Item 'c:\Users\DELL\.gemini\antigravity\brain\c2a0bbe7-5235-47fb-a401-9a7844ce009e\uploaded_media_3_1770756057846.png' 'img\actividad1\evidence4.png' -Force
+Copy-Item 'c:\Users\DELL\.gemini\antigravity\brain\c2a0bbe7-5235-47fb-a401-9a7844ce009e\uploaded_media_4_1770756057846.png' 'img\actividad1\evidence5.png' -Force
+Copy-Item 'c:\Users\DELL\.gemini\antigravity\brain\c2a0bbe7-5235-47fb-a401-9a7844ce009e\uploaded_media_1770757949772.png' 'img\actividad1\evidence6.png' -Force
